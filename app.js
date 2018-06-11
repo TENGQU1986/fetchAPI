c923a125334ca7b9c6ae66955b36d5f3658a6663
Patrick Qu <quteng.bo@gmail.com>
2018-06-11 12:47:54 -0400

    get api data

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+
+// Get local text file
 function getText() {
   fetch('text.txt')
     .then(function(res) {
-      console.log(res);
-  });
+      return res.text();
+  })
+  .then((data) => {
+    // console.log(data);
+    document.getElementById('output').innerHTML = data;
+  })
+  .catch((err) => console.log(err))
+}
+
+// Get local JOSN file
+
+function getJson() {
+  fetch('posts.json')
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let output = [];
+      data.forEach((post) => {
+        output += `<li>${post.title}</li>`;
+        document.getElementById('output').innerHTML = output;
+        console.log(output);
+      });
+      
+    })
+}
+
+// GET API Data
+
+function getExternal() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let output = [];
+      data.forEach((post) => {
+        output += `<li>${post.id}</li>`;
+        document.getElementById('output').innerHTML = output;
+        console.log(output);
+    });
+})
 }
